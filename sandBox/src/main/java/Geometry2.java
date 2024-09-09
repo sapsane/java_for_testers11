2a0fbc552c728a19e9255e92c89d930a2d418f17
Rotanov01 <rotanov.ev@gmail.com>
2024-09-09 21:02:47 +0300

    эксперименты с функциями 2

--- sandBox/src/main/java/Geometry2.java
+++ sandBox/src/main/java/Geometry2.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Geometry2 {
+    public static void main(String[] args) {
+        var side=7.0;
+        System.out.println("площадь квадрата со строной " +side + " = " + SquareArea(side));
+        printSquareArea(7.0);
+        printSquareArea(5.0);
+        printSquareArea(9.1);
+        printRectangleArea(3.0, 5.0);
+        printRectangleArea(7.0,9.0);
+    }
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("площадь прямоугольника со стронами " + a + " и " + b + " = " + RectangleArea(a,b));
+    }
+
+    private static double RectangleArea(double a, double b) {
+        return a * b;
+    }
+
+    static void printSquareArea (double side) {
+        System.out.println("площадь квадрата со строной " +side + " = " + SquareArea(side));
+
+    }
+
+    private static double SquareArea(double a) {
+        return a * a;
+    }
 }
--- sandBox/src/main/java/HELLO.java
+++ sandBox/src/main/java/HELLO.java
@@ -1,5 +1,10 @@
 public class HELLO {
     public static void main(String[] args) {
         System.out.println("Hello world ! ! !");
+
+        System.out.println( "2+3="+(2+3));
+        System.out.println("Hello 2 "+"world");
+
+
     }
 }
