ea7fca43ca78c902f3759bad40810486466f1466
Rotanov01 <rotanov.ev@gmail.com>
2024-09-13 19:57:21 +0300

    исправленное ДЗ=2

--- sandBox/src/main/java/ru/STQA/geometry/figures/Triangle.java
+++ sandBox/src/main/java/ru/STQA/geometry/figures/Triangle.java
@@ -1,8 +1,21 @@
 package ru.STQA.geometry.figures;
 
 public class Triangle {
-    public static void printTrianglePerimeter(double a1, double b1, double c1) {
-        String text2 = String.format("функция периметр треугольника с стороной a=%f b=%f c=%f Perimetr=%f ", a1, b1, c1, perimetr(a1, b1, c1));
+
+    double sideA= 3.0;
+    double sideB= 4.0;
+    double sideC= 5.0;
+
+    public Triangle() {
+        this.sideA=sideA;
+        this.sideB=sideB;
+        this.sideC=sideC;
+
+           }
+
+
+    public static void printTrianglePerimeter(Triangle tr1) {
+        String text2 = String.format("Объект периметр треугольника с стороной a=%f b=%f c=%f Perimetr=%f ", 3.0, 4.0,5.0, tr1.perimetr());
         System.out.println(text2);
 
     }
@@ -11,15 +24,23 @@ public class Triangle {
         return a1 + b1 + c1;
     }
 
-    public static double printTriangleArea(double sideA, double sideB, double sideC) {
+    public static double printTriangleArea(Triangle tr2) {
 
         String text = String.format(
-                "функция площадь треугольника с стороной a= %f  b= %f c= %f  S=%f", sideA, sideB, sideC, Math.sqrt(halfPerimetr(sideA, sideB, sideC) * (halfPerimetr(sideA, sideB, sideC) - sideA) * (halfPerimetr(sideA, sideB, sideC) - sideB) * (halfPerimetr(sideA, sideB, sideC) - sideC)));
+                "объект площадь треугольника с стороной a= %f  b= %f c= %f  S=%f", tr2.sideA, tr2.sideB, tr2.sideC  , tr2.printTriangleArea1());
         System.out.println(text);
-        return Math.sqrt(halfPerimetr(sideA, sideB, sideC) * (halfPerimetr(sideA, sideB, sideC) - sideA) * (halfPerimetr(sideA, sideB, sideC) - sideB) * (halfPerimetr(sideA, sideB, sideC) - sideC));
+        return tr2.printTriangleArea1();
     }
 
     public static double halfPerimetr(double a1, double b1, double c1) {
-           return perimetr(a1, b1, c1) / 2;
-       }
+        return perimetr(a1, b1, c1) / 2;
+    }
+
+    public double perimetr() {
+        return this.sideA+this.sideB+this.sideC;
+    }
+
+    public double printTriangleArea1() {
+        return Math.sqrt(halfPerimetr(this.sideA, this.sideB, this.sideC) * (halfPerimetr(this.sideA, this.sideB, this.sideC) - this.sideA) * (halfPerimetr(this.sideA, this.sideB, this.sideC) - this.sideB) * (halfPerimetr(this.sideA, this.sideB, this.sideC) - this.sideC));
+    }
 }
--- sandBox/src/test/java/ru/STQA/geometry/figures/TriangleTests.java
+++ sandBox/src/test/java/ru/STQA/geometry/figures/TriangleTests.java
@@ -8,10 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TriangleTests {
     @Test
     void canCalculateArea(){
-        assertEquals(6.0, Triangle.printTriangleArea(3.0,4.0,5.0));
+        var s = new Triangle(3.0,4.0,5.0);
+        assertEquals(6.0, s.printTriangleArea1());
     }
     @Test
     void canCalculatePerimetr(){
-        assertEquals(12.0, Triangle.perimetr(3, 4, 5));
+        assertEquals(12.0, new Triangle(3.0,4.0,5.0).perimetr());
     }
 }
