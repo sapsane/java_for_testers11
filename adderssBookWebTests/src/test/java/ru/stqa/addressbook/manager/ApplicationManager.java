ad7f6b550fc80037377f87e13e6a4150d7aca280
Rotanov01 <rotanov.ev@gmail.com>
2024-11-01 20:11:49 +0300

    Задание №13 попытка1

--- adderssBookWebTests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
+++ adderssBookWebTests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
     public static WebDriver driver;
   //  protected  WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
     private ContactHelper contacts;
+    private Properties properties;
 
 
     public LoginHelper session(){
@@ -37,7 +40,9 @@ public class ApplicationManager {
     }
 
 
-   public void init(String browser) {
+   public void init(String browser, Properties properties) {
+        this.properties=properties;
+
         if (driver == null) {
          if ("firefox".equals(browser)) {
                 driver = new FirefoxDriver();
@@ -47,9 +52,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser %s",browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseURL"));
             driver.manage().window().setSize(new Dimension(945, 979));
-            session().login("admin", "secret" );
+            session().login(properties.getProperty("web.username") , properties.getProperty("web.password") );
 
         }
     }
--- adderssBookWebTests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ adderssBookWebTests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -3,17 +3,25 @@ package ru.stqa.addressbook.tests;
 import ru.stqa.addressbook.manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
     protected static ApplicationManager app;
 
 
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         if (app==null){
+            var properties= new Properties();
+            properties.load(new FileReader(System.getProperty("target","local.properties")));
             app=new ApplicationManager();
+            app.init(System.getProperty("browser","firefox"),properties);
         }
-        app.init(System.getProperty("browser","firefox"));
+
     }
 
 
